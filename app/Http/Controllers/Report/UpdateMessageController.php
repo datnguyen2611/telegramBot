23dfd88eddcca1ec25b0b66690435f18c2e0067b
datnguyen26 <datnguyen26111997@gmail.com>
2020-02-27 16:30:06 +0700

    update code

--- app/Http/Controllers/Report/UpdateMessageController.php
+++ app/Http/Controllers/Report/UpdateMessageController.php
@@ -8,6 +8,7 @@ use Longman\TelegramBot\Telegram;
 use DB;
 use Carbon\Carbon;
 
+
 class UpdateMessageController extends Controller
 {
 
@@ -18,11 +19,11 @@ class UpdateMessageController extends Controller
         $bot_api_key = '1065422651:AAG5xftZNiwBVs8-c40qVpOood1K-tg6qGY';
         $bot_username = 'TheSmartMovebot';
         $mysql_credentials = [
-            'host' => 'localhost',
-            'port' => 3306, // optional
-            'user' => 'root',
-            'password' => '',
-            'database' => 'telegrambot',
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'), // optional
+            'user' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'database' => env('DB_DATABASE'),
         ];
         try {
             // Create Telegram API object
@@ -31,30 +32,27 @@ class UpdateMessageController extends Controller
             $telegram->enableMySql($mysql_credentials);
             $data = $telegram->handleGetUpdates();
             //check buoi toi
-            $like_text_1 = "cong viec cua em";
-            $like_text_2 = "thoi gian thuc te";
             $day = 'sáng';
-            $message_buoi_toi = $this->checkReport($day, $like_text_1, $like_text_2);
-            $this->sendmessage($message_buoi_toi);
-            //check buoi sang
-//            $message_buoi_sang = $this->checkReport($buoi, $like_text_1, $like_text_2);
-//            $buoi = "sang";
-//            $this->sendmessage($message_buoi_sang);
+            $chatId = '-549030259';
+            $checkreport = $this->checkReport($day, $chatId);
+            $this->sendmessage($checkreport);
         } catch (\Longman\TelegramBot\Exception\TelegramException $e) {
             // log telegram errors
             // echo $e->getMessage();
         }
     }
-    private function sendmessage($message)
+
+    private function sendmessage($message ,$chatId)
     {
         $a = new \Longman\TelegramBot\Request();
         $result = $a->sendMessage([
-            'chat_id' => -290352932,
+            'chat_id' => $chatId,
             'text' => $message,
         ]);
         return;
     }
-    private function checkReport($day, $like_text_1, $like_text_2)
+
+    private function checkReport($day, $chatId)
     {
         $dateZone = Carbon::now('Asia/Ho_Chi_Minh');
         $today = $dateZone->toDateString();
@@ -63,47 +61,137 @@ class UpdateMessageController extends Controller
             ->join('user', 'user.id', 'message.user_id')
             ->join('chat', 'chat.id', 'message.chat_id')
             ->whereDate('date', $dateZone)
-            ->where('message.chat_id', '-339631790')
-//            ->where('message.text', 'like', "%$like_text_1%")
-//            ->orWhere('message.text', 'like', "%$like_text_2%")
-            ->select(['user.id', 'user.first_name', 'user.last_name', 'message.text', 'chat_id'])
+            ->where('message.chat_id', $chatId)
+            ->select(['user.id', 'user.first_name', 'user.last_name', 'message.text', 'chat_id', 'message.date'])
             ->get()->toArray();
+        $userReportSang = [];
+        $userReportToi = [];
 
-        $user_has_report = [];
         foreach ($data as $val) {
-            if ($realTime > '09:00:00' && strpos("$val->text","$like_text_1")  !== false ) {
-                $day = 'sáng';
-                $user_has_report[] = $val->id;
-
+            $isMorning = $this->checkBaoCaoBuoiSang($val->text, $val->date);
+            $isAfternoon = $this->checkBaoCaoBuoiChieu($val->text, $val->date);
+//            $userReportSang[] = $isMorning;
+            if ($isMorning) {
+                $userReportSang[] = $val->id;
             }
-           elseif($realTime > '16:00:00' && strpos("$val->text","$like_text_2")  !== false) {
-                $day = 'tối';
-                $user_has_report[] = $val->id;
+            if ($isAfternoon) {
+                $userReportToi[] = $val->id;
 
             }
         }
-//        echo "<pre>";
-//        print_r($user_has_report);
-//        echo $day;
-//        die();
 
-
-        $message = $this->getUsersNotReport($day, $user_has_report);
+//        $message = $this->getUsersNotReport($day, $chatId , $userReportSang , $userReportToi);
+        $message = $this->getUsersNotReport($day, $chatId,$userReportSang,$userReportToi);
+        echo "<pre>";
+        print_r($message);
+        die();
         return $message;
     }
 
-
-    private function getUsersNotReport($day, $ids)
+    private function checkTimeSang($time1, $time2)
     {
-        $data = DB::table('user')
-            ->whereNotIn('id', $ids)
-            ->get()->toArray();
-        $message = "";
-        foreach ($data as $val) {
-            $text = $val->last_name ." ". $val->first_name . " : chưa bảo cáo buổi $day" . "\n";
-            $message .= $text;
+        $time1 = Carbon::parse($time1);
+        $time2 = Carbon::parse($time2);
+       return  $time1->lt($time2);
+    }
+
+    private function checkTimeToi($time1, $time2)
+    {
+        $time1 = Carbon::parse($time1);
+        $time2 = Carbon::parse($time2);
+        return $time1->gt($time2);
+    }
+
+    private function checkBaoCaoBuoiSang($message, $timeMessage)
+    {
+        $flag = false;
+        $message = $this->vn_to_str($message);
+        $message = strtolower($message);
+        $message = trim(preg_replace('/\s+/', ' ', $message));
+        $text = "cong_viec_cua_em";
+        $before12 = $this->checkTimeSang($timeMessage, '09:00:00');
+        if (strpos("$message", "$text") !== false && $before12 !== false) {
+            $flag = true;
         }
+        return $flag;
+    }
+
+    private function checkBaoCaoBuoiChieu($message, $timeMessage)
+    {
+        $flag = false;
+        $message = $this->vn_to_str($message);
+        $message = strtolower($message);
+        $message = trim(preg_replace('/\s+/', ' ', $message));
+        $text = "cong_viec_hoan_thanh";
+        $before12 = $this->checkTimeToi($timeMessage, '15:00:00');
+
+        if (strpos("$message", "$text") !== false && $before12 !== false) {
+            $flag = true;
+        }
+        return $flag;
+    }
+
+    private function vn_to_str($str)
+    {
+        $unicode = array(
+            'a' => 'á|à|ả|ã|ạ|ă|ắ|ặ|ằ|ẳ|ẵ|â|ấ|ầ|ẩ|ẫ|ậ',
+            'd' => 'đ',
+            'e' => 'é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ',
+            'i' => 'í|ì|ỉ|ĩ|ị',
+            'o' => 'ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ',
+            'u' => 'ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự',
+            'y' => 'ý|ỳ|ỷ|ỹ|ỵ',
+            'A' => 'Á|À|Ả|Ã|Ạ|Ă|Ắ|Ặ|Ằ|Ẳ|Ẵ|Â|Ấ|Ầ|Ẩ|Ẫ|Ậ',
+            'D' => 'Đ',
+            'E' => 'É|È|Ẻ|Ẽ|Ẹ|Ê|Ế|Ề|Ể|Ễ|Ệ',
+            'I' => 'Í|Ì|Ỉ|Ĩ|Ị',
+            'O' => 'Ó|Ò|Ỏ|Õ|Ọ|Ô|Ố|Ồ|Ổ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ở|Ỡ|Ợ',
+            'U' => 'Ú|Ù|Ủ|Ũ|Ụ|Ư|Ứ|Ừ|Ử|Ữ|Ự',
+            'Y' => 'Ý|Ỳ|Ỷ|Ỹ|Ỵ',
+        );
+        foreach ($unicode as $nonUnicode => $uni) {
+            $str = preg_replace("/($uni)/i", $nonUnicode, $str);
+        }
+        $str = str_replace(' ', '_', $str);
+        return $str;
+    }
+
+    private function getUsersNotReport($day, $chatId,$userNotReportSang,$userNotReportToi)
+    {
+        $data = DB::table('user_chat')
+            ->join('user', 'user.id', 'user_chat.user_id')
+            ->join('chat', 'chat.id', 'user_chat.chat_id')
+            ->where('user.is_bot', 0)
+            ->where('has_report', 0)
+            ->where('user_chat.chat_id', $chatId)
+            ->select('user.first_name', 'user.last_name');
+//            ->get()->toArray();
+        $userNotReportSang = $data->whereNotIn('user.id', $userNotReportSang)->get()->toArray();
+        $userNotReportToi = $data->whereNotIn('user.id', $userNotReportToi)->get()->toArray();
+        $message = "";
+//        $dateZone = Carbon::now('Asia/Ho_Chi_Minh');
+        $timeZone = Carbon::now()->toTimeString();
+        if ($userNotReportSang && $timeZone <'09:00:00'){
+            foreach ($userNotReportSang as $val) {
+                $day = 'Sáng';
+                $text = $val->first_name . " " . $val->last_name . " : chưa bảo cáo buổi $day" . "\n";
+                $message .= $text;
+            }
+        }
+        if ($userNotReportToi && $timeZone >'20:00:00'){
+            foreach ($userNotReportToi as $val) {
+                $day = 'Tối';
+                $text = $val->first_name . " " . $val->last_name . " : chưa bảo cáo buổi $day" . "\n";
+                $message .= $text;
+            }
+        }
+//        foreach ($data as $val) {
+//            $text = $val->first_name . " " . $val->last_name . " : chưa bảo cáo buổi $day" . "\n";
+//            $message .= $text;
+//        }
         echo "Sussess!";
         return $message;
     }
+
+
 }
